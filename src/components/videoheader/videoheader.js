069e74401a30f50916f702b3e64b00fba69e0845
ZackHolby <zackholbrook44@gmail.com>
2021-05-10 10:23:22 -0400

    added images and built out file structure

---
+++ src/components/videoheader/videoheader.js
@@ -0,0 +1,25 @@
+import video from './images/siteVideoEdited2MB.mp4';
+import './videoheader.css';
+
+function videoheader() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and saying my name - Holby.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default videoheader;
