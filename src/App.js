9594ccbb34e2df5040f4126a34aa8e8969d15089
ZackHolby <zackholbrook44@gmail.com>
2021-05-13 00:35:39 -0400

    added project section outline with new images and titles

--- src/App.js
+++ src/App.js
@@ -16,6 +16,7 @@ class App extends React.Component {
         <Videoheader />
         <About />
         <Experience />
+        <Projects />
       </div>
     );
   }
--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -2,7 +2,12 @@ import './Projects.css';
 import React from "react";
 import { render } from '@testing-library/react';
 import Card from "../card/Card"
-import img1 from "../../images/blackjack.png"
+import RecipeImg from "../../images/recipegenerator_min.jpg"
+import BlackjackImg from "../../images/blackjack.png"
+import PersonalSiteImg from "../../images/personalSite.png"
+import InterpreterImg from "../../images/interpreterPic.png"
+import DataImg from "../../images/shotdata_min.jpg"
+import OutlookImg from "../../images/outlook.png"
 
 
 class Projects extends React.Component {
@@ -12,13 +17,22 @@ class Projects extends React.Component {
                 <div className="container-fluid d-flex justify-content-center">
                     <div class="row">
                         <div class="col-lg-4 offset-lg-0 col-md-4 offset-md-0 col-sm-10 offset-sm-1 col-10 offset-1">
-                            <Card pic={CapitalOneImg} title="Software Engineering Intern" text=""></Card>
+                            <Card pic={RecipeImg} title="Recipe and Music Generator" text=""></Card>
                         </div>
                         <div class="col-lg-4 offset-lg-0 col-md-4 offset-md-0 col-sm-10 offset-sm-1 col-10 offset-1">
-                            <Card pic={GeImg} title="Digital Technology Intern" text=""></Card>
+                            <Card pic={InterpreterImg} title="Custom Interpreter" text=""></Card>
                         </div>
                         <div class="col-lg-4 offset-lg-0 col-md-4 offset-md-0 col-sm-10 offset-sm-1 col-10 offset-1">
-                            <Card pic={Tenet3Img} title="Cyber Security Intern" text=""></Card>
+                            <Card pic={BlackjackImg} title="Online Blackjack" text=""></Card>
+                        </div>
+                        <div class="col-lg-4 offset-lg-0 col-md-4 offset-md-0 col-sm-10 offset-sm-1 col-10 offset-1">
+                            <Card pic={PersonalSiteImg} title="Personal site" text=""></Card>
+                        </div>
+                        <div class="col-lg-4 offset-lg-0 col-md-4 offset-md-0 col-sm-10 offset-sm-1 col-10 offset-1">
+                            <Card pic={DataImg} title="Data Mining" text=""></Card>
+                        </div>
+                        <div class="col-lg-4 offset-lg-0 col-md-4 offset-md-0 col-sm-10 offset-sm-1 col-10 offset-1">
+                            <Card pic={OutlookImg} title="Email Automation Script" text=""></Card>
                         </div>
                     </div>
                 </div>
