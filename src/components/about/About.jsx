d77a7eaa12ca7c95425c7f75cb1f17a2652cac6d
ZackHolby <zackholbrook44@gmail.com>
2021-05-14 01:08:19 -0400

    removed extra pictures for storage and added more card bodies

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -20,13 +20,13 @@ class About extends React.Component {
                 <div className="container-fluid d-flex justify-content-center">
                     <div class="row">
                         <div class="col-lg-4 offset-lg-0 col-md-4 offset-md-0 col-sm-10 offset-sm-1 col-10 offset-1">
-                            <Card pic={CSEpic} title="Computer Science" text="Graduated Cum Laude in May 2021 with B.S. Computer Science and Engineering from Ohio State. I grew particularly interested in Web Dev and Data/AI."></Card>
+                            <Card pic={CSEpic} title="Computer Science" text="Graduated Cum Laude in May 2021 with B.S. Computer Science and Engineering from The Ohio State University. I grew particularly interested in Web Dev and Data/AI."></Card>
                         </div>
                         <div class="col-lg-4 offset-lg-0 col-md-4 offset-md-0 col-sm-10 offset-sm-1 col-10 offset-1">
-                            <Card pic={basketballPic} title="Activity" text="I practiced with the Ohio State Women's Basketball team where I was part of the Scout team. Outside of that, I continued to play tennis and spikeball."></Card>
+                            <Card pic={basketballPic} title="Activity" text="I practiced with the Ohio State Women's Basketball team where I was part of the Scout team. Outside of that, I continued to play tennis and spikeball all around campus."></Card>
                         </div>
                         <div class="col-lg-4 offset-lg-0 col-md-4 offset-md-0 col-sm-10 offset-sm-1 col-10 offset-1">
-                            <Card pic={vballPic} title="Interests" text="I have strong interests in Cryptocurrency and the stock market analysis. I research Philosophy and its governence within society - specifically morality and ethics."></Card>
+                            <Card pic={vballPic} title="Interests" text="I have strong interests in Cryptocurrency and the stock market analysis. I research Philosophy and its governence within society and technology - specifically morality and ethics."></Card>
                         </div>
                     </div>
                 </div>
--- src/components/experience/Experience.jsx
+++ src/components/experience/Experience.jsx
@@ -17,13 +17,13 @@ class Experience extends React.Component {
                 <div className="container-fluid d-flex justify-content-center">
                     <div class="row">
                         <div class="col-lg-4 offset-lg-0 col-md-4 offset-md-0 col-sm-10 offset-sm-1 col-10 offset-1">
-                            <Card pic={CapitalOneImg} title="Software Engineering Intern" text=""></Card>
+                            <Card pic={CapitalOneImg} title="Software Engineering Intern" text="I was a Software Engineering intern on the Software Resiliency team. Tech Stack: Angular, Python, AWS, TerraForm, and Snowflake."></Card>
                         </div>
                         <div class="col-lg-4 offset-lg-0 col-md-4 offset-md-0 col-sm-10 offset-sm-1 col-10 offset-1">
-                            <Card pic={GeImg} title="Digital Technology Intern" text=""></Card>
+                            <Card pic={GeImg} title="Digital Technology Intern" text="I was a Digital Technology intern apart of the MyApps team. Tech Stack: Angular, Node.js, Express, Bootstrap, Karma, Docker, New Relic. "></Card>
                         </div>
                         <div class="col-lg-4 offset-lg-0 col-md-4 offset-md-0 col-sm-10 offset-sm-1 col-10 offset-1">
-                            <Card pic={Tenet3Img} title="Cyber Security Intern" text=""></Card>
+                            <Card pic={Tenet3Img} title="Cyber Security Intern" text="I was a Cyber Security intern on the MeTRA team. Technologies used: Python, Jupyter Notebook, REST API, Docker.  "></Card>
                         </div>
                     </div>
                 </div>
