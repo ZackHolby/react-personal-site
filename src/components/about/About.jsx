7f2c5e312e6e4b64e93c94c4651fd58a09f64e34
ZackHolby <zackholbrook44@gmail.com>
2021-05-14 00:47:41 -0400

    added bodies to About me section

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -20,13 +20,13 @@ class About extends React.Component {
                 <div className="container-fluid d-flex justify-content-center">
                     <div class="row">
                         <div class="col-lg-4 offset-lg-0 col-md-4 offset-md-0 col-sm-10 offset-sm-1 col-10 offset-1">
-                            <Card pic={CSEpic} title="Computer Science" text="Body 1"></Card>
+                            <Card pic={CSEpic} title="Computer Science" text="Graduated Cum Laude in May 2021 with B.S. Computer Science and Engineering from Ohio State. I grew particularly interested in Web Dev and Data/AI."></Card>
                         </div>
                         <div class="col-lg-4 offset-lg-0 col-md-4 offset-md-0 col-sm-10 offset-sm-1 col-10 offset-1">
-                            <Card pic={basketballPic} title="Activity" text="Body 1"></Card>
+                            <Card pic={basketballPic} title="Activity" text="I practiced with the Ohio State Women's Basketball team where I was part of the Scout team. Outside of that, I continued to play tennis and spikeball."></Card>
                         </div>
                         <div class="col-lg-4 offset-lg-0 col-md-4 offset-md-0 col-sm-10 offset-sm-1 col-10 offset-1">
-                            <Card pic={vballPic} title="Interests" text="Body 3"></Card>
+                            <Card pic={vballPic} title="Interests" text="I have strong interests in Cryptocurrency and the stock market analysis. I research Philosophy and its governence within society - specifically morality and ethics."></Card>
                         </div>
                     </div>
                 </div>
